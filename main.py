f8f7aa8903fc8a5b1a65115dcc64396e5e5b4ee7
takar <takartin@gmail.com>
2021-08-04 09:03:51 +0900

    vggnet 사용

--- main.py
+++ main.py
@@ -5,58 +5,65 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # 헬퍼(helper) 라이브러리를 임포트합니다
 import numpy as np
 import matplotlib.pyplot as plt
-
-# image_generator = ImageDataGenerator(
-#             rotation_range=10, # 회전
-#             zoom_range=0.10, # 확대
-#             shear_range=0.5, # 기울임
-#             width_shift_range=0.10, # 가로방향 평행 이동
-#             height_shift_range=0.10, # 세로방향 평행 이동
-#             horizontal_flip=True, # 좌우반전
-#             vertical_flip=False # 상하반전
-# )
-
-augment_size = 100
-
-fashion_mnist = keras.datasets.fashion_mnist
-
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+from matplotlib.pyplot import imshow
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-# for i in range(train_images):
-#   train_images = image_generator.flow(np.tile(train_images[i].reshape(28*28),10).reshape(-1,28,28,1),
-#                            np.zeros(augment_size), batch_size=augment_size,
-#                            shuffle=False).next()[i]
-train_images = train_images.reshape(-1, 28, 28, 1)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
 train_images = train_images / 255.0
-test_images = test_images.reshape(10000, 28, 28, 1)
 test_images = test_images / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(64, (2,2), activation='relu', input_shape=(28, 28, 1)),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(64, (2,2), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2,2),
-  tf.keras.layers.Dropout(0.15),
+train_images = train_images.reshape(-1, 28, 28, 1)
+test_images = test_images.reshape(-1, 28, 28, 1)
+
+image_generator = ImageDataGenerator(
+  rotation_range=10,
+  zoom_range=0.10,
+  shear_range=0.5,
+  width_shift_range=0.10,
+  height_shift_range=0.10,
+  horizontal_flip=True,
+  vertical_flip=False)
+
+augment_size = 30000
+
+randidx = np.random.randint(train_images.shape[0], size=augment_size)
+x_augmented = train_images[randidx].copy()
+y_augmented = train_labels[randidx].copy()
+x_augmented = image_generator.flow(x_augmented, np.zeros(augment_size), batch_size=augment_size, shuffle=False).next()[
+  0]
+
+train_images = np.concatenate((train_images, x_augmented))
+train_labels = np.concatenate((train_labels, y_augmented))
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Conv2D(input_shape=(28, 28, 1), kernel_size=(3, 3), filters=32, padding='same', activation='relu'),
+  tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=64, padding='same', activation='relu'),
+  tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+  tf.keras.layers.Dropout(rate=0.5),
+  tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=128, padding='same', activation='relu'),
+  tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=256, padding='valid', activation='relu'),
+  tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+  tf.keras.layers.Dropout(rate=0.5),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
+  tf.keras.layers.Dense(units=512, activation='relu'),
+  tf.keras.layers.Dropout(rate=0.5),
+  tf.keras.layers.Dense(units=256, activation='relu'),
+  tf.keras.layers.Dropout(rate=0.5),
+  tf.keras.layers.Dense(units=10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file :
-    json_file.write(model_json)
+model.fit(train_images, train_labels, epochs=50)
 
-
-
-model.fit(train_images, train_labels, epochs=5)
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
 print('\n테스트 정확도:', test_acc)
 
@@ -64,8 +71,6 @@ predictions = model.predict(test_images)
 
 predictions[0]
 
-model.save_weights("model.h5")
-print("Saved model to disk")
 
 def plot_image(i, predictions_array, true_label, img): #번호,예측배열,정답,이미지
   predictions_array, true_label, img = predictions_array[i], true_label[i], img[i] #예상된 배열값, 정답, 이미지
@@ -125,34 +130,3 @@ for i in range(num_images):
   plot_value_array(i, predictions, test_labels)
 plt.show()
 
-plt.subplot()
-
-fig, (ax1,ax2) =plt.subplots(1, 2, figsize= (12,5))
-
-y_vloss = history.history['val_loss']
-
-y_loss = history.history['loss']
-
-x_len = np.arange(len(y_loss))
-ax1.plot(x_len, y_vloss, marker ='.', c="red", label='Testset_loss')
-ax1.plot(x_len,y_loss,marker='.',c='blue', label = 'Trainset_loss')
-
-ax1.legend(loc='upper right')
-ax1.grid()
-ax1.set(xlable='epoch', ylabel='loss')
-
-y_vaccuracy = history.history['val_accuracy']
-
-y_accuracy = history.history['accuracy']
-
-x_len = np.arange(len(y_accuracy))
-ax2.plot(x_len, y_vaccuracy, marker = '.', c="red", label='Testset_accuracy')
-ax2.plot(x_len, y_accuracy, marker = '.', c='blue', label = 'Trainset_accuracy')
-
-ax2.legend(loc='lower right')
-ax2.grid()
-
-ax2.set(xlabel='epoch', ylabel='accuracy')
-
-ax2.grid(True)
-plt.show()
